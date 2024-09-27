3446cf085599434f83f12ffeecf8797367e4a0c0
Nguyễn Hoàng Đăng Khoa <23520749@gm.uit.edu.vn>
2024-09-28 00:08:23 +0700

    Khoa generate .ui .py file for ui image_captuer_view

--- View/Image_Capture_View.py
+++ View/Image_Capture_View.py
@@ -6,13 +6,14 @@ from PyQt5.uic import loadUi
 from typing import Protocol
 from cv2.typing import MatLike
 
+from View.ui_Image_Capture_View import Ui_Image_Capture_View
 
 
 
-
-class ImageCaptureView(QWidget):
+class ImageCaptureView(QWidget, Ui_Image_Capture_View ):
     ICV_back_button_signal = pyqtSignal()
     ICV_next_button_signal = pyqtSignal()
+    
     def __init__(self) -> None:
         super().__init__()
         # super(ImageCaptureView, self).__init__()
@@ -20,25 +21,27 @@ class ImageCaptureView(QWidget):
         # '''self.setWindowTitle("Camera App with MVP Architecture")
 
         # Thiết lập các thành phần GUI
-        self.setWindowTitle("Image Capture View")
-        self.setGeometry(0,0,450,800)
-        self.setMinimumSize(450,800)
-        self.initUI()
+        
+        self.setupUi(self)
+        # self.setWindowTitle("Image Capture View")
+        # self.setGeometry(0,0,450,800)
+        # self.setMinimumSize(450,800)
+        # self.initUI()
         
         
-    def initUI(self) -> None:
-        self.back_button = QPushButton("Back", self)
+        # def initUI(self) -> None:
+        # self.back_button = QPushButton("Back", self)
         self.back_button.clicked.connect(self.emit_back_button_clicked_signal)
-        self.back_button.setGeometry(25,25,50,50)
+        # self.back_button.setGeometry(25,25,50,50)
         
-        self.preview_image_label = QLabel(self)
-        self.preview_image_label.setGeometry(0,100,450,600)
+        # self.preview_image_label = QLabel(self)
+        # self.preview_image_label.setGeometry(0,100,450,600)
         
-        self.preview_fps_label = QLabel(self)
-        self.preview_fps_label.setGeometry(25,700,50,50)
+        # self.preview_fps_label = QLabel(self)
+        # self.preview_fps_label.setGeometry(25,700,50,50)
         
-        self.next_button = QPushButton("Next", self)
-        self.next_button.setGeometry(100,700,200,50)
+        # self.next_button = QPushButton("Next", self)
+        # self.next_button.setGeometry(100,700,200,50)
         self.next_button.clicked.connect(self.emit_next_button_clicked_signal)
 
     #slot
@@ -56,7 +59,6 @@ class ImageCaptureView(QWidget):
         self.preview_image_label.setPixmap(QPixmap.fromImage(image).scaled(450,600))
 
 
-        
     def update_preview_fps_gui(self, fps: float) -> None:
         # self.fps.setText(f"FPS: {fps}")
         self.preview_fps_label.setText(f"FPS: {fps}")
---
+++ View/ui_Image_Capture_View.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'Image_Capture_ViewTgRpWQ.ui'
+##
+## Created by: Qt User Interface Compiler version 6.7.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PyQt5.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PyQt5.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PyQt5.QtWidgets import (QApplication, QLabel, QPushButton, QSizePolicy,
+    QWidget)
+import View.UI_ViewResource_rc
+
+class Ui_Image_Capture_View(object):
+    def setupUi(self, Image_Capture_View):
+        if not Image_Capture_View.objectName():
+            Image_Capture_View.setObjectName(u"Image_Capture_View")
+        Image_Capture_View.resize(450, 800)
+        self.next_button = QPushButton(Image_Capture_View)
+        self.next_button.setObjectName(u"next_button")
+        self.next_button.setGeometry(QRect(375, 25, 50, 50))
+        icon = QIcon()
+        icon.addFile(u":/View/Icon/right_arrow.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.next_button.setIcon(icon)
+        self.next_button.setIconSize(QSize(50, 50))
+        self.preview_fps_label = QLabel(Image_Capture_View)
+        self.preview_fps_label.setObjectName(u"preview_fps_label")
+        self.preview_fps_label.setGeometry(QRect(25, 425, 50, 50))
+        self.preview_fps_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.capture_button = QPushButton(Image_Capture_View)
+        self.capture_button.setObjectName(u"capture_button")
+        self.capture_button.setGeometry(QRect(200, 425, 50, 50))
+        icon1 = QIcon()
+        icon1.addFile(u":/View/Icon/Capture_button.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.capture_button.setIcon(icon1)
+        self.capture_button.setIconSize(QSize(40, 40))
+        self.preview_image_label = QLabel(Image_Capture_View)
+        self.preview_image_label.setObjectName(u"preview_image_label")
+        self.preview_image_label.setGeometry(QRect(25, 100, 400, 300))
+        self.preview_image_label.setStyleSheet(u"background-color: rgb(85, 255, 127);")
+        self.preview_image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.back_button = QPushButton(Image_Capture_View)
+        self.back_button.setObjectName(u"back_button")
+        self.back_button.setGeometry(QRect(25, 25, 50, 50))
+        icon2 = QIcon()
+        icon2.addFile(u":/View/Icon/left_arrow.png", QSize(), QIcon.Mode.Normal, QIcon.State.Off)
+        self.back_button.setIcon(icon2)
+        self.back_button.setIconSize(QSize(50, 50))
+
+        self.retranslateUi(Image_Capture_View)
+
+        QMetaObject.connectSlotsByName(Image_Capture_View)
+    # setupUi
+
+    def retranslateUi(self, Image_Capture_View):
+        Image_Capture_View.setWindowTitle(QCoreApplication.translate("Image_Capture_View", u"Form", None))
+        self.next_button.setText("")
+        self.preview_fps_label.setText(QCoreApplication.translate("Image_Capture_View", u"FPS", None))
+        self.capture_button.setText("")
+        self.preview_image_label.setText("")
+        self.back_button.setText("")
+    # retranslateUi
+
