6deddad2a55b4a67a150b43e8d423b3ffc155977
Nguyễn Hoàng Đăng Khoa <23520749@gm.uit.edu.vn>
2024-10-30 17:06:25 +0700

    Khoa add alert message box

--- Test/test4.py
+++ Test/test4.py
@@ -1,88 +1,75 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QPushButton, QLabel, QScrollArea, QHBoxLayout, QStackedWidget, QSizePolicy
-from PyQt5.QtCore import QSize, QRect, QCoreApplication, QMetaObject
-from PyQt5.QtGui import QPalette, QColor
+from PyQt5.QtWidgets import QApplication, QWidget, QStackedWidget, QVBoxLayout, QPushButton, QLabel, QMessageBox
 
-class Ui_Form(object):
-    def setupUi(self, Form):
-        if not Form.objectName():
-            Form.setObjectName(u"Form")
-        Form.resize(558, 978)
-        Form.setMaximumSize(QSize(1038, 1938))
-        self.horizontalLayout_2 = QHBoxLayout(Form)
-        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
-        self.scrollArea = QScrollArea(Form)
-        self.scrollArea.setObjectName(u"scrollArea")
-        self.scrollArea.setMinimumSize(QSize(540, 960))
-        self.scrollArea.setWidgetResizable(True)
-        self.scrollAreaWidgetContents = QWidget()
-        self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
-        self.scrollAreaWidgetContents.setGeometry(QRect(-572, -992, 1098, 1938))
-        self.horizontalLayout = QHBoxLayout(self.scrollAreaWidgetContents)
-        self.horizontalLayout.setObjectName(u"horizontalLayout")
-        self.stackedWidget = QStackedWidget(self.scrollAreaWidgetContents)
-        self.stackedWidget.setObjectName(u"stackedWidget")
-        self.stackedWidget.setMinimumSize(QSize(1080, 1920))
-        self.page = QWidget()
-        self.page.setObjectName(u"page")
-        self.stackedWidget.addWidget(self.page)
-        self.page_2 = QWidget()
-        self.page_2.setObjectName(u"page_2")
-        self.stackedWidget.addWidget(self.page_2)
+class FirstWidget(QWidget):
+    def __init__(self, stack):
+        super().__init__()
+        self.stack = stack
+        self.initUI()
 
-        self.horizontalLayout.addWidget(self.stackedWidget)
+    def initUI(self):
+        layout = QVBoxLayout()
+        self.label = QLabel('This is the first widget', self)
+        self.label.setStyleSheet("background-color: lightblue;")
+        layout.addWidget(self.label)
 
-        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.button = QPushButton('Go to Second Widget', self)
+        self.button.clicked.connect(self.showDialog)
+        layout.addWidget(self.button)
 
-        self.horizontalLayout_2.addWidget(self.scrollArea)
+        self.setLayout(layout)
 
-        self.retranslateUi(Form)
-        QMetaObject.connectSlotsByName(Form)
+    def showDialog(self):
+        reply = QMessageBox.question(self, 'Message', 'Are you sure to go to the second widget?',
+                                     QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
 
-    def retranslateUi(self, Form):
-        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        if reply == QMessageBox.Yes:
+            self.stack.setCurrentIndex(1)
 
-class MainWindow(QMainWindow):
+class SecondWidget(QWidget):
+    def __init__(self, stack):
+        super().__init__()
+        self.stack = stack
+        self.initUI()
+
+    def initUI(self):
+        layout = QVBoxLayout()
+        self.label = QLabel('This is the second widget', self)
+        self.label.setStyleSheet("background-color: lightgreen;")
+        layout.addWidget(self.label)
+
+        self.button = QPushButton('Go to First Widget', self)
+        self.button.clicked.connect(self.showDialog)
+        layout.addWidget(self.button)
+
+        self.setLayout(layout)
+
+    def showDialog(self):
+        reply = QMessageBox.question(self, 'Message', 'Are you sure to go to the first widget?',
+                                     QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+
+        if reply == QMessageBox.Yes:
+            self.stack.setCurrentIndex(0)
+
+class AppDemo(QWidget):
     def __init__(self):
         super().__init__()
-        self.ui = Ui_Form()
-        self.ui.setupUi(self)
+        self.setWindowTitle('QStackedWidget Example')
+        self.setGeometry(100, 100, 300, 200)
 
-        widget1 = QWidget()
-        layout1 = QVBoxLayout(widget1)
-        button1 = QPushButton("Go to Widget 2", widget1)
-        button1.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(1))
-        label1 = QLabel("Widget 1", widget1)
-        label1.setFixedSize(400, 400)
-        label1.setAutoFillBackground(True)
-        palette1 = label1.palette()
-        palette1.setColor(QPalette.Window, QColor("lightblue"))
-        label1.setPalette(palette1)
-        layout1.addWidget(button1)
-        layout1.addWidget(label1)
+        self.stack = QStackedWidget(self)
+        self.firstWidget = FirstWidget(self.stack)
+        self.secondWidget = SecondWidget(self.stack)
 
-        widget2 = QWidget()
-        layout2 = QVBoxLayout(widget2)
-        button2 = QPushButton("Go to Widget 1", widget2)
-        button2.clicked.connect(lambda: self.ui.stackedWidget.setCurrentIndex(0))
-        label2 = QLabel("Widget 2", widget2)
-        label2.setFixedSize(400, 400)
-        label2.setAutoFillBackground(True)
-        palette2 = label2.palette()
-        palette2.setColor(QPalette.Window, QColor("lightgreen"))
-        label2.setPalette(palette2)
-        layout2.addWidget(button2)
-        layout2.addWidget(label2)
+        self.stack.addWidget(self.firstWidget)
+        self.stack.addWidget(self.secondWidget)
 
-        self.ui.stackedWidget.addWidget(widget1)
-        self.ui.stackedWidget.addWidget(widget2)
-
-        # Set the scroll area to expand with the main window
-        self.ui.scrollArea.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        self.ui.scrollAreaWidgetContents.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        layout = QVBoxLayout()
+        layout.addWidget(self.stack)
+        self.setLayout(layout)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    mainWin = MainWindow()
-    mainWin.show()
+    demo = AppDemo()
+    demo.show()
     sys.exit(app.exec_())
